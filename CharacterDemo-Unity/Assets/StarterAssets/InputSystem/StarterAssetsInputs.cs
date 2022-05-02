43285243b78e2c53f11af551c5856a90cc6e6d3d
aschia <ajs1173@utulsa.edu>
2022-05-02 16:24:43 -0500

    Character Game Demo

---
+++ CharacterDemo-Unity/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+
+		private Animator _animator;
+		//private int _animIDIdle;
+		private int _animIDWalk;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+#if !UNITY_IOS || !UNITY_ANDROID
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+#endif
+
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+#else
+        // old input sys if we do decide to have it (most likely wont)...
+#endif
+
+        private void Start()
+        {
+			_animator = GetComponent<Animator>();
+			//_animIDIdle = Animator.StringToHash("Idle");
+			_animIDWalk = Animator.StringToHash("Walk");
+		}
+
+        public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+			if (move.x != 0 || move.y != 0) _animator.SetBool(_animIDWalk, true);
+			else _animator.SetBool(_animIDWalk, false);
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+
+#if !UNITY_IOS || !UNITY_ANDROID
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+
+#endif
+
+	}
+	
+}
